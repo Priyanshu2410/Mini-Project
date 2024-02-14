4d8d1be33c85478e2107f24ce7c5853e5031efb3
Priyanshu Patel <patelpriysnshu2410@gmail.com>
2024-02-14 15:19:27 +0530

    sidebar update

--- src/Pages/user/home.jsx
+++ src/Pages/user/home.jsx
@@ -12,28 +12,39 @@ const Home = () => {
   }, []);
 
   return (
-    <div className="pt-[87px] bg-[#d6d4d5] bg-opacity-80 ">
-      <div className="flex mt-10 ml-5">
-        <div className='flex items-center'>
-          <span className='text-3xl font-bold mr-3'>Welcome,</span>
-          <h1 className='text-3xl font-bold'>{user.firstName}</h1>
+    <div>
+      <div className="pt-[87px] bg-[#d6d4d5] bg-opacity-80 h-[800px] ">
+        <div className="flex mt-10 ml-5">
+          <div className='flex items-center'>
+            <span className='text-3xl font-bold mr-3'>Welcome,</span>
+            <h1 className='text-3xl font-bold'>{user.firstName}</h1>
+          </div>
         </div>
-      </div>
-      <div className="flex justify-end items-center font-sans">
-        <div className="flex flex-col items-center">
-          <h1 className="text-[50px] font-bold max-w-[800px]">Learn today’s most in-demand
-            skills with our free online
-            courses</h1>
-          <button className="bg-[#3047b8] text-white font-bold text-xl px-3 py-2 rounded-full max-w-[300px] mt-5">Explore Our Best courses</button>
+        <div className="flex justify-end items-center font-sans">
+          <div className="flex flex-col items-center">
+            <h1 className="text-[50px] font-bold max-w-[800px]">Learn today’s most in-demand
+              skills with our free online
+              courses</h1>
+            <button className="bg-[#3047b8] text-white font-bold text-xl px-3 py-2 rounded-full max-w-[300px] mt-5">Explore Our Best courses</button>
+          </div>
+          <img src="/userhome.png" alt="" className="ml-5 w-[550px]" />
         </div>
-        <img src="/userhome.png" alt="" className="ml-5 w-[550px]" />
+
+
+
       </div>
-      <div className="mt-[180px]">
-        <RunSidebar />
-      </div>
+
+      <div className="mt-[20px] text-center">
+  <h1 className="text-4xl font-bold text-gray-700 transition-all duration-300 hover:text-[#1a4e63] hover:transform hover:translate-y-1">
+    Collage of Brilliance: Where Expert Minds Converge
+  </h1>
+  <RunSidebar />
+</div>
+
 
 
     </div>
+
   );
 };
 
--- src/component/runsidebar.jsx
+++ src/component/runsidebar.jsx
@@ -2,25 +2,25 @@ import React from "react";
 import "../styles/animated.css";
 
 const RunSidebar = () => {
-  const images = ["/userhome.png", "/CVM logo.png", "/CVM logo.png", "/CVM logo.png", "/CVM logo.png", "/CVM logo.png"];
+  const images = [, "/CVM logo.png", "/ADIT.png", "/gcet.png", "/mbit.png","/samcom.png","/aribas1.png" ];
 
   return (
     <div className=" flex items-center mt-[50px] mb-12 justify-center">
       {/* 1. */}
       <div className="w-[200%] h-[180px] flex items-center border-t border-b border-gray-600 overflow-hidden relative">
         {/* 2. */}
-        <div className="w-[200%] flex items-center h-20 justify-around absolute left-0 animate gap-20 animate">
+        <div className="w-[200%] flex items-center h-20 justify-around absolute left-0 animate gap-20 animate p-10">
           {/* 3 */}
           {images.map((i) => {
             return (
-              <div className=" items-start w-[60rem]">
+              <div className=" items-start w-[40rem]">
                 <img src={i} />
               </div>
             );
           })}
           {images.map((i) => {
             return (
-              <div className=" items-start w-[60rem]">
+              <div className=" items-start w-[40rem]">
                 <img src={i} />
               </div>
             );
