ec4cd2ff29ac26836c0a1e72a87771e49e5c470e
ptlraj <rajvp1103@gmail.com>
2024-03-15 22:55:34 +0530

    updateprof

--- src/Pages/user/UserProfile.jsx
+++ src/Pages/user/UserProfile.jsx
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { CgClose } from "react-icons/cg";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const UserProfile = () => {
   const [user, setUser] = useState({});
@@ -25,25 +28,26 @@ const UserProfile = () => {
       console.log("user", user.email);
 
       const result = await axios.put(
-        `http://localhost:5000/update-profile/${user.email}`,{
-            first_name : firstName,
-            last_name : lastName,
-            number:phone,
-            gender:gender,
-            collage:college,
-            department:dept
+        `http://localhost:5000/update-profile/${user.email}`,
+        {
+          first_name: firstName,
+          last_name: lastName,
+          number: phone,
+          gender: gender,
+          collage: college,
+          department: dept,
         }
       );
-        setUser({
-            firstName : firstName,
-            lastName : lastName,
-            phone:phone,
-            gender:gender,
-            college:college,
-            dept:dept
-        })
-        localStorage.setItem("data",JSON.stringify(result.data))
-        console.log(result);
+      setUser({
+        firstName: firstName,
+        lastName: lastName,
+        phone: phone,
+        gender: gender,
+        college: college,
+        dept: dept,
+      });
+      localStorage.setItem("data", JSON.stringify(result.data));
+      console.log(result);
     } catch (error) {
       if (error.response && error.response.status === 400) {
         // Handle the case where the email is already registered
@@ -56,7 +60,7 @@ const UserProfile = () => {
       }
     }
     setshow(false);
-    alert("profile update successfully");
+    toast.success("Updated Successfully");
   };
 
   return (
@@ -92,101 +96,116 @@ const UserProfile = () => {
               </button>
               {show ? (
                 <>
-                <div className="fixed inset-0 bg-gray-700 bg-opacity-90"></div>
+                  <div className="fixed inset-0 bg-gray-700 bg-opacity-90"></div>
 
                   <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 max-w-30rem">
-                    <form onSubmit={handleSubmit}>
-                      <input
-                        className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded"
-                        type="text"
-                        placeholder="Enter your firstName"
-                        onChange={(e) => setFirstName(e.target.value)}
-                        value={firstName}
-                        required
-                      />
-                      <input
-                        className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mt-4"
-                        type="text"
-                        placeholder="Enter your lastname"
-                        onChange={(e) => setLastName(e.target.value)}
-                        value={lastName}
-                        required
-                      />
-                      <input
-                        className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mt-4"
-                        type="phone"
-                        placeholder="Enter your phone number"
-                        onChange={(e) => setPhone(e.target.value)}
-                        value={phone}
-                        required
-                      />
-                      <select
-                        name="gender"
-                        required
-                        className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mt-4"
-                        onChange={(e) => setGender(e.target.value)}
-                  
-                      >
-                        <option value="" disabled selected>
-                          Select gender
-                        </option>
-                        <option value="male">Male</option>
-                        <option value="female">Female</option>
-                        <option value="other">Other</option>
-                      </select>
-                      <select
-                        name="collage"
-                        required
-                        className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mt-4"
-                        onChange={(e) => setCollage(e.target.value)}
-                      >
-                        <option value="" disabled selected>
-                          Select your collage
-                        </option>
-                        <option value="A. D. Patel Institute of Technology">
-                          ADIT
-                        </option>
-                        <option value="G H Patel College of Engineering & Technology">
-                          GCET
-                        </option>
-                        <option value="Madhuben & Bhanubhai Patel Institute of Technology">
-                          MBIT
-                        </option>
-                        <option value="other">OTHER</option>
-                      </select>
-                      <select
-                        name="dept"
-                        required
-                        className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mt-4"
-                        onChange={(e) => setDept(e.target.value)}
-                      >
-                        <option value="" disabled selected>
-                          Select your department
-                        </option>
-                        <option value="Information Technology">
-                          Information Technology
-                        </option>
-                        <option value="Computer Enginnering">
-                          Computer Engineering{" "}
-                        </option>
-                        <option value="Data Science and Artificial Intelligence">
-                          Artificial Intelligence & Data Science (AI&DS)
-                        </option>
-                        <option value="Computer Enginnering and Design">
-                          Computer Engineering and design
-                        </option>
-                        <option value="other">Other</option>
-                      </select>
-                      <div className="mt-4 flex justify-between font-semibold text-sm"></div>
-                      <div className="text-center md:text-left">
-                        <button
-                          className="mt-4 bg-blue-600 hover:bg-blue-700 px-4 py-2 text-white uppercase rounded text-xs tracking-wider"
-                          type="submit"
+                    <div className="bg-white rounded-lg p-5">
+                      <div className=" text-black pb-5 flex flex-row justify-between">
+                        <h1>Update Your Profile</h1>
+                        <div
+                          onClick={() => setshow(false)}
+                          className="text-end"
                         >
-                          done
-                        </button>
+                          <CgClose />
+                        </div>
                       </div>
-                    </form>
+                      <form onSubmit={handleSubmit}>
+                        <input
+                          className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded"
+                          type="text"
+                          placeholder="Enter your firstName"
+                          onChange={(e) => setFirstName(e.target.value)}
+                          value={firstName}
+                          required
+                        />
+                        <input
+                          className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mt-4"
+                          type="text"
+                          placeholder="Enter your lastname"
+                          onChange={(e) => setLastName(e.target.value)}
+                          value={lastName}
+                          required
+                        />
+                        <input
+                          className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mt-4"
+                          type="phone"
+                          placeholder="Enter your phone number"
+                          onChange={(e) => setPhone(e.target.value)}
+                          value={phone}
+                          required
+                        />
+                        <select
+                          name="gender"
+                          required
+                          className="text-sm w-full px-4 py-2 border border-solid invalid:text-gray-500 border-gray-300 rounded mt-4"
+                          onChange={(e) => setGender(e.target.value)}
+                        >
+                          <option
+                            value=""
+                            disabled
+                            selected
+                            className="text-gray-500"
+                          >
+                            Select gender
+                          </option>
+                          <option value="male">Male</option>
+                          <option value="female">Female</option>
+                          <option value="other">Other</option>
+                        </select>
+                        <select
+                          name="collage"
+                          required
+                          className="text-sm w-full px-4 py-2 border border-solid invalid:text-gray-500 border-gray-300 rounded mt-4"
+                          onChange={(e) => setCollage(e.target.value)}
+                        >
+                          <option value="" disabled selected>
+                            Select your collage
+                          </option>
+                          <option value="A. D. Patel Institute of Technology">
+                            ADIT
+                          </option>
+                          <option value="G H Patel College of Engineering & Technology">
+                            GCET
+                          </option>
+                          <option value="Madhuben & Bhanubhai Patel Institute of Technology">
+                            MBIT
+                          </option>
+                          <option value="other">OTHER</option>
+                        </select>
+                        <select
+                          name="dept"
+                          required
+                          className="text-sm w-full px-4 py-2 border border-solid border-gray-300 invalid:text-gray-500 rounded mt-4"
+                          onChange={(e) => setDept(e.target.value)}
+                        >
+                          <option value="" disabled selected>
+                            Select your department
+                          </option>
+                          <option value="Information Technology">
+                            Information Technology
+                          </option>
+                          <option value="Computer Enginnering">
+                            Computer Engineering{" "}
+                          </option>
+                          <option value="Data Science and Artificial Intelligence">
+                            Artificial Intelligence & Data Science (AI&DS)
+                          </option>
+                          <option value="Computer Enginnering and Design">
+                            Computer Engineering and design
+                          </option>
+                          <option value="other">Other</option>
+                        </select>
+                        <div className="mt-4 flex justify-between font-semibold text-sm"></div>
+                        <div className="text-center md:text-left">
+                          <button
+                            className="mt-4 bg-blue-600 hover:bg-blue-700 px-4 py-2 text-white uppercase rounded text-xs tracking-wider"
+                            type="submit"
+                          >
+                            done
+                          </button>
+                        </div>
+                      </form>
+                    </div>
                   </div>
                 </>
               ) : null}
@@ -194,6 +213,7 @@ const UserProfile = () => {
           </div>
         </div>
       </div>
+      <ToastContainer />
     </div>
   );
 };
